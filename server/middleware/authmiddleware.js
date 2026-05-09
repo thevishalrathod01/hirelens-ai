fe3d4a23851845a5dda5649ec140d0acc5875354
The Vishal Rathod <deathclickervishal01@gmail.com>
2026-05-09 22:08:35 +0530

    C:\Users\death\Desktop\hirelens-ai\server\middleware\authmiddleware.js

--- server/middleware/authmiddleware.js
+++ server/middleware/authmiddleware.js
@@ -23,6 +23,7 @@ export const protect = async (req, res, next) => {
     req.user = await User.findById(decoded.id).select("-password")
 
     next()
+
   } catch (error) {
     res.status(401).json({
       message: "Not authorized, token failed",
